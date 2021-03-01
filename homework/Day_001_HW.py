418ae41ff1744a1e9b6274515eb3a3a92d527469
linhn0617 <79580533+linhn0617@users.noreply.github.com>
2021-03-01 15:28:18 +0800

    2021.03.01

---
+++ homework/Day_001_HW.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import numpy as np
+np.arange(0,21,1)
+
+
+# In[11]:
+
+
+import numpy as np
+np.arange(2, 21, 2)
+
+
+# In[12]:
+
+
+import numpy as np
+np.arange(3, 21, 3)
+
