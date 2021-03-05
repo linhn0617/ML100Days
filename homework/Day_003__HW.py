7929ce4192daf219def3d23feb3fe1ec8fbf590a
linhn0617 <79580533+linhn0617@users.noreply.github.com>
2021-03-05 22:40:29 +0800

    2021.03.05

---
+++ homework/Day_003__HW.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import numpy as np
+v1 = 20000
+v2 = 20
+result = 20*np.log10(v1/v2)
+print(result)
+
+
+# In[16]:
+
+
+import numpy as np
+V0 = 20
+V1_30 = 10**(30/20)*V0
+V1_50 = 10**(50/20)*V0
+V1_30/V1_50
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
